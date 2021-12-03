dd80adcc69fa291eb414c7c25f9412a9a7889d8d
leizhuo <1369544613@qq.com>
2021-12-03 18:53:44 +0800

    初始化

---
+++ src/test/java/cn/edu/zut/vacc/VaccApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zut.vacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
