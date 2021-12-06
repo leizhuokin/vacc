55d303fb4da970f5bb9793c69d7a9613a025785d
wzh <1024044696@qq.com>
2021-12-06 10:31:44 +0800

    增删改查

--- src/main/java/cn/edu/zut/vacc/App.java
+++ src/main/java/cn/edu/zut/vacc/App.java
@@ -1,8 +1,9 @@
 package cn.edu.zut.vacc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("cn.edu.zut.*.mapper")
 @SpringBootApplication
 public class App {
 
