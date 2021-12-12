c304b44b756e52c82a1d030f331f35677d088df5
leizhuo <1369544613@qq.com>
2021-12-12 17:04:39 +0800

    登录实现

--- src/main/java/cn/edu/zut/vacc/App.java
+++ src/main/java/cn/edu/zut/vacc/App.java
@@ -1,18 +1,28 @@
 package cn.edu.zut.vacc;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @ServletComponentScan
 @MapperScan("cn.edu.zut.*.mapper")
 @SpringBootApplication
+@Configuration
 public class App {
-
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
 
 }
--- src/main/java/cn/edu/zut/vacc/controller/IndexController.java
+++ src/main/java/cn/edu/zut/vacc/controller/IndexController.java
@@ -2,11 +2,17 @@ package cn.edu.zut.vacc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @GetMapping("/login")
     public String login(){
-        return "/login";
+        return "login";
+    }
+    @RequestMapping("/queryUser")
+    public String queryUser(){
+        return "queryUser";
     }
 }
